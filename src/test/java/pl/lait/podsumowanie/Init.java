f1dba9bf86bc3f08d2ac697e7e9da7998aa699a5
GryMich <GryMich@gmail.com>
2018-12-08 12:45:16 +0100

    init commit

---
+++ src/test/java/pl/lait/podsumowanie/Init.java
@@ -0,0 +1,19 @@
+package pl.lait.podsumowanie;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Init {
+
+	static WebDriver driver; 
+	
+	public static WebDriver getDriver() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MICHAŁ\\chromedriver.exe");
+		
+		driver = new ChromeDriver ();
+		driver.get("https://www.seleniumhq.org");
+		
+		return driver; 
+	}
+}
