7dfd341e5664f604cc88937651cd569663f6b635
GryMich <GryMich@gmail.com>
2018-12-09 14:08:55 +0100

    uzupełnienie o testy zakładek

---
+++ src/test/java/pl/lait/podsumowanie/Common.java
@@ -0,0 +1,41 @@
+package pl.lait.podsumowanie;
+
+import static org.junit.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByLinkText;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Common {
+	
+	static WebDriver driver;
+	
+	public static void goToMainPage() {
+		System.out.println("Uruchamiam przeglądarkę");
+		driver = Init.getDriver();
+	}
+	
+	public static void linkClick(String linkText) {
+		System.out.println("Klikam w link: "+linkText);
+		try {
+			driver.findElement(By.linkText(linkText)).click();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("Generuję print screen z błędem");
+			Init.printScr(driver);
+			assertTrue("Nie znalazłem linku: " + 
+			linkText, false);
+		} 
+	}
+	
+	public static void checkPageTitleAndStopTestIfWrong(String title) {
+		boolean checkStatus = driver.getTitle().equals(title);
+		assertTrue("Trytuł strony jest nieprawidłowy", checkStatus);
+	}
+	
+	public static WebElement findInputByName(String name) {
+		return driver.findElement(By.name(name));
+	}
+}
--- src/test/java/pl/lait/podsumowanie/TopMenuTest.java
+++ src/test/java/pl/lait/podsumowanie/TopMenuTest.java
@@ -1,37 +1,71 @@
 package pl.lait.podsumowanie;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.Timestamp;
+import static org.junit.Assert.assertTrue;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.Augmenter;
 
-public class TopMenuTest {
+public class TopMenuTest extends Common {
 
 	@Test
 	public void topMenu() {
-		String testName = Thread.currentThread().getStackTrace()[1].getMethodName();
+		goToMainPage();
 		
-		WebDriver driver = Init.getDriver();
-		Init.printScr(driver, "TopMenuTest", testName);
-		driver.findElement(By.linkText("About")).click();
-		Init.printScr(driver, "TopMenuTest", testName);
-		//Init.printScr(driver);
-		driver.quit();
+		linkClick("About");
+		linkClick("Support");
+		linkClick("Documentation");
+		linkClick("Download");
+		linkClick("Projects");
+		findInputByName("q").sendKeys("szukany tekst");
+		String h2xpath = "//*[@id=\"mainContent\"]/h2";
+		String h2text = driver.findElement(By.xpath(h2xpath)).getText();
+		System.out.println("######+h2text");
+		if(h2text.equals("Selenium Projects")) {
+			System.out.println("Znaleziono poprawny " + "komunikat na stronie");
+		}
+		
+		//driver.quit();
 	}
 	
-	@Test
+	/*@Test
 	public void topMenu2() {
-		
 		WebDriver driver = Init.getDriver();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
 		driver.findElement(By.linkText("Support")).click();
 		Init.printScr(driver);
+		
+		title = driver.getTitle();
+		
+		System.out.println(title);
+		
+		assertTrue("Page title is wrong!", title.equals("Getting Help"));
+
 		driver.quit();
-	}
+	}*/
+	
+	/*@Test
+	public void topMenu2wrongTitle() {
+		WebDriver driver = Init.getDriver();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		driver.findElement(By.linkText("Support")).click();
+		Init.printScr(driver);
+		
+		title = driver.getTitle();
+		
+		System.out.println(title);
+		
+		assertTrue("Page title is wrong!", title.equals("Some wrong page title"));
+
+		driver.quit();
+	}*/
+	
+	
+
 }
