f493d5a71a67d93d99f728b89231926bf95af95f
GryMich <GryMich@gmail.com>
2018-12-09 09:13:07 +0100

    Dodane screenshoty

--- src/test/java/pl/lait/podsumowanie/Init.java
+++ src/test/java/pl/lait/podsumowanie/Init.java
@@ -1,7 +1,15 @@
 package pl.lait.podsumowanie;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.Augmenter;
 
 
 public class Init {
@@ -16,4 +24,36 @@ public class Init {
 		
 		return driver; 
 	}
+
+	public static void printScr(WebDriver driver) {
+		
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis()); // timestamp to zmienna, która przechowuje date 
+		Long milis = timestamp.getTime(); // pobiera aktualny czas w milisekundach
+
+		WebDriver driver_tmp = new Augmenter().augment(driver); 
+		File srcFile = ((TakesScreenshot)driver_tmp).getScreenshotAs(OutputType.FILE); // to robi screenshoty
+
+		try {
+			FileUtils.copyFile(srcFile, new File("target/Screeny/screenshot-"+milis+".png")); //w targecie będą pojawiały się screenshoty z nazwa w milisekundach w formacie png
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printScr(WebDriver driver, String folderName, String testName) {
+		
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis()); // timestamp to zmienna, która przechowuje date 
+		Long milis = timestamp.getTime(); // pobiera aktualny czas w milisekundach
+
+		WebDriver driver_tmp = new Augmenter().augment(driver); 
+		File srcFile = ((TakesScreenshot)driver_tmp).getScreenshotAs(OutputType.FILE); // to robi screenshoty
+
+		try {
+			FileUtils.copyFile(srcFile, new File("target/"+ folderName+"/"+ testName +"-" + milis + ".png")); //w targecie będą pojawiały się screenshoty z nazwa w milisekundach w formacie png
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 }
--- src/test/java/pl/lait/podsumowanie/TopMenuTest.java
+++ src/test/java/pl/lait/podsumowanie/TopMenuTest.java
@@ -1,15 +1,37 @@
 package pl.lait.podsumowanie;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmenter;
 
 public class TopMenuTest {
 
 	@Test
 	public void topMenu() {
+		String testName = Thread.currentThread().getStackTrace()[1].getMethodName();
 		
 		WebDriver driver = Init.getDriver();
+		Init.printScr(driver, "TopMenuTest", testName);
 		driver.findElement(By.linkText("About")).click();
+		Init.printScr(driver, "TopMenuTest", testName);
+		//Init.printScr(driver);
+		driver.quit();
+	}
+	
+	@Test
+	public void topMenu2() {
+		
+		WebDriver driver = Init.getDriver();
+		driver.findElement(By.linkText("Support")).click();
+		Init.printScr(driver);
+		driver.quit();
 	}
 }
